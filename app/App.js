bdc9262262249dedea87c12766fc4a863857c342
KuljeetJ <kuljeetjhala7@gmail.com>
2016-03-29 03:35:02 +0530

    Integrated JSDoc module for Java Documentation.

--- app/App.js
+++ app/App.js
@@ -3,6 +3,10 @@ export default class Bapp extends Component {
   foo() {
     return 'good';
   }
+
+ /**
+  * This function re-renders the components whenever it's state changes.
+  */
   render() {
     return (
       <div className="commentBox">
