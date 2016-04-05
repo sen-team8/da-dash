588f230a7d02835c70a120ad91c76aada845e3c9
KuljeetJ <kuljeetjhala7@gmail.com>
2016-04-05 21:49:27 +0530

    Added test cases for reducers

---
+++ tests/Redux/loginActions.test.js
@@ -0,0 +1,34 @@
+import nock from 'nock';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import * as actions from '../../app/redux/loginActions';
+import { expect } from 'chai';
+const user ={
+  id: '201301030@daiict.ac.in',
+  pass: 'dawebmailsucks',
+};
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
+
+describe('async login actions', () => {
+  afterEach(() => {
+    nock.cleanAll();
+  });
+
+  it('Get login', function foo(done) {
+    this.timeout(10000);
+
+    const expectedActions = [
+      { type: 'LOGGING' },
+      { type: 'LOGGED_IN' },
+    ];
+
+    const store = mockStore({ reducer: { intranet: {} } });
+    store.dispatch(actions.verifyUser(user))
+     .then(() => { // return of async actions
+       expect(store.getActions()).to.deep.equal(expectedActions);
+     })
+     .then(done) // test passed
+     .catch(done); // test failed
+  });
+});
