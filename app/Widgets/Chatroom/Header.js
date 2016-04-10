a243c2bc795f231fe0d0dfc948c4a7fb1a3b906a
Barkha Bhojak <Barkha Bhojak>
2016-04-10 15:25:18 +0530

    updated ui

--- app/Widgets/Chatroom/Header.js
+++ app/Widgets/Chatroom/Header.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Button } from 'react-bootstrap';
+import { Button, ButtonToolbar } from 'react-bootstrap';
 import { Link } from 'react-router';
 
 const buttonStyle = {
-  height: '45px',
-  width: '100px',
-  marginLeft: '10px',
+  height: '38px',
+  width: '90px',
+  float: 'right',
 };
 
 let buttonPressed ='1';
@@ -31,17 +31,23 @@ export default class MyComponent extends React.Component {
         height: '45px',
         fontSize: '20px',
         display: 'flex',
-        flexDirection: 'row',
-        justifyContent: 'centre',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        marginTop: '10px',
+        marginBottom: '10px',
+        borderBottomStyle: 'solid',
+        borderColor: '#d3d3d3',
+        borderWidth: '2px',
       }}
       >
-        <div>
-          <Link to="chatroom" style= {{ paddingTop: '5px' }}>Chat Application</Link>
+        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+        <Link to="chatroom" style= {{ paddingTop: '5px', fontSize: '24px' }}>Chat Application</Link>
+        <ButtonToolbar>
+          <Button id="1" style= {buttonStyle} onClick={this.onToggle} active={this.props.batch} >Your Batch</Button>
+          <Button id="2" style= {buttonStyle} onClick={this.onToggle} active={!this.props.batch}>Da-iict</Button>
+        </ButtonToolbar>
         </div>
-        <Button id="1" style= {buttonStyle} onClick={this.onToggle} active={this.props.batch} >Your Batch</Button>
-        <Button id="2" style= {buttonStyle} onClick={this.onToggle} active={!this.props.batch}>Da-iict</Button>
-
-    </div>
+        </div>
     );
   }
 }
