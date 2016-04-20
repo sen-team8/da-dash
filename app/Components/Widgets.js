30a3a324615c925a9832e61377ff8dc3c002b5a9
Barkha Bhojak <Barkha Bhojak>
2016-04-20 18:20:45 +0530

    updated choose widgets page
    login page

--- app/Components/Widgets.js
+++ app/Components/Widgets.js
@@ -7,10 +7,12 @@ import { ListGroup } from 'react-bootstrap';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { actions } from '../redux/actions';
-import { Grid, Row, Col } from 'react-bootstrap';
+import { Grid, Row, Col, Panel } from 'react-bootstrap';
 
 const style = {
   width: 400,
+  justifyContent: 'center',
+  alignItems: 'center',
 };
 
 class Container extends Component {
@@ -40,9 +42,27 @@ class Container extends Component {
   render() {
     const { widgets } = this.props;
     return (
+      <div style={{ backgroundColor: '#F5FCFF',
+                    width: '100%',
+                    height: '100%',
+                    display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'center',
+                    alignItems: 'center' }}
+      >
       <Grid>
       <Row>
       <Col offset={6}>
+      <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+      <Panel defaultExpanded header="Choose Widgets" style={{ width: '700px' }}>
+      Drag and drop widgets to customize your dashboard.
+      You can also select widgets to show on your home screen by checking or unchecking boxes.
+      <div style={{ display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    marginTop: '10px' }}
+      >
         <ListGroup style={style}>
           {widgets.map((widget, i) => {
             return (
@@ -57,9 +77,13 @@ class Container extends Component {
             );
           })}
         </ListGroup>
+        </div>
+        </Panel>
+        </div>
       </Col>
       </Row>
       </Grid>
+      </div>
     );
   }
 }
